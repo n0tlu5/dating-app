0276e73ed4badf08304b4b88137093129b599281
n0tlu5 <darfisultoni@gmail.com>
2024-12-07 15:01:41 +0700

    chore: update migration file path

--- dating-app-backend/data-source.ts
+++ dating-app-backend/data-source.ts
@@ -8,9 +8,9 @@ const dataSource = new DataSource({
   password: 'password',
   database: 'dating_app',
   entities: ['dist/**/*.entity.js'], // Path to compiled entities
-  migrations: ['dist/migrations/*.js'], // Path to compiled migrations
+  migrations: ['dist/src/migrations/*.js'], // Path to compiled migrations
   synchronize: false, // Use migrations for schema changes
   logging: true,
 });
 
-export default dataSource;
+export default dataSource
